55f6612a29edf795d32e67936d7c8ab61c9a544c
Mikhail Volkov <freylis2@gmail.com>
2018-03-05 22:23:25 +0500

    Publish to pypi

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+
+setup(
+    name='yadisk-api',
+    version='1.0',
+    packages=['yadisk_api'],
+    url='https://github.com/freylis/yadisk-api',
+    license='MIT License',
+    author='Mikhail Volkov',
+    author_email='freylis2@gmail.com',
+    description='Yandex.disk http api client',
+    long_description=README,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Cloud service',
+    ],
+    install_requires = [
+        'requests >= 2.9.1',
+    ],
+)
